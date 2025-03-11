35a644bb451863de1dacd53ae4e05c3007e1f1a1
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-11 22:18:51 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//*Kullanıcıdan aldığınız şifreninin geçerli olup olmadığını aşağıdaki kurallara
+// göre kontrol ederek "Geçerli Şifre" ya da "Geçersiz Şifre" şeklinde dönen kodu yazınız.
+//  1-En az 8 karakter içermeli
+//  2-Space karakteri içermemeli
+//  3-İlk harf büyük harf olmalı,
+//  4-Son karakteri ? olmalı *//
+
+        Scanner scanner =new Scanner(System.in);
+        while (true) {
+            System.out.print("Şifreyi giriniz: ");
+            String sifre = scanner.nextLine();
+            if (sifre.length() < 8) {
+                System.out.println("❌Lütfen 8 ve üstü karakterde şifre deneyiniz!");
+            } else if (sifre.contains(" ")) {
+                System.out.println("❌Lütfen boşluk kullanmadan deneyiniz!");
+            } else if (!Character.isUpperCase(sifre.charAt(0))) {
+                System.out.println("❌Lütfen ilk harfi büyük karakter olarak deneyiniz!");
+            } else if (sifre.charAt(sifre.length() - 1) == '?') {
+                System.out.println("✅......... Şifre Başarılı........ !");
+                break;
+            } else {
+                System.out.println("❌Lütfen son karakter ? olarak deneyiniz!");
+            }
         }
     }
 }
+
